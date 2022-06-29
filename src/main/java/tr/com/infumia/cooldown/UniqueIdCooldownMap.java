dd439ca6a7e6b7485db138431603885a240f61fc
Hasan Demirtaş <utsukushihito@outlook.com>
2022-06-29 17:49:52 +0300

    files. (#2)

---
+++ src/main/java/tr/com/infumia/cooldown/UniqueIdCooldownMap.java
@@ -0,0 +1,28 @@
+package tr.com.infumia.cooldown;
+
+import java.util.UUID;
+import lombok.experimental.Delegate;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * an interface to determine unique id cooldown maps.
+ */
+public interface UniqueIdCooldownMap extends CooldownMap<UUID> {
+  /**
+   * creates a new composed cooldown map.
+   *
+   * @param base the base to create.
+   *
+   * @return a newly creates composed cooldown map instance.
+   */
+  @NotNull
+  static UniqueIdCooldownMap create(@NotNull final Cooldown base) {
+    return new Impl(CooldownMap.create(base));
+  }
+
+  /**
+   * a record class that represents simple implementation of {@link UniqueIdCooldownMap}.
+   */
+  record Impl(@NotNull @Delegate CooldownMap<UUID> delegate)
+    implements UniqueIdCooldownMap {}
+}
